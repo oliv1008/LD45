9da535ec18ad4661e7f6c21feb130db4fa8da5c8
valentin-roche <35733063+valentin-roche@users.noreply.github.com>
2019-10-06 12:33:55 +0200

    sceneloader base

--- singleton/SceneLoader.gd
+++ singleton/SceneLoader.gd
@@ -1,24 +1,11 @@
 extends Node
 
-#The currently active scene
-var currentScene = null
-
 func _ready():
-	#On load set the current scene to the last scene available
-	currentScene = get_tree().get_root().get_child(get_tree().get_root().get_child_count() -1)
+	pass
    
 # create a function to switch between scenes 
-func setScene(scene):
-	#clean up the current scene
-	if(currentScene != $main):
-		currentScene.queue_free()
-	#load the file passed in as the param "scene"
-	var s = ResourceLoader.load(scene)
-	#create an instance of our scene
-	currentScene = s.instance()
-	# add scene to root
-	get_tree().get_root().add_child(currentScene)
-	
-	print("launching scene : " + scene)
-
-
+func setScene(target, scene):
+	target.get_child(0).queue_free()
+	var resource = ResourceLoader.load(scene)
+	print(scene)
+	target.add_child(resource.instance())
