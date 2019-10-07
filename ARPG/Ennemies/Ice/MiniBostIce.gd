ef3cf4f065509d135d3c3d49fca4e942afa69915
valentin-roche <35733063+valentin-roche@users.noreply.github.com>
2019-10-07 17:44:40 +0200

    fix intégration

--- ARPG/Ennemies/Ice/MiniBostIce.gd
+++ ARPG/Ennemies/Ice/MiniBostIce.gd
@@ -37,7 +37,7 @@ func _physics_process(delta):
 			look_at(playerPos)
 			velocity = posToMove.normalized() * mouvementSpeed
 			move_and_collide(velocity*delta)
-	if raycast.is_colliding() && raycast.get_collider().player == true:
+	if raycast.is_colliding() && raycast.get_collider().get_name() == "Player":
 		isAttacking = true
 		attack()
 
@@ -83,9 +83,9 @@ func _on_Area2D_body_entered(body):
 	if body == self:
 		pass
 	else:
-		if (body.has_method("get_hit") && body.player == true):
+		if (body.has_method("get_hit") && body.get_name() == "Player"):
 			body.get_hit()
 
 func _on_Agro_body_entered(body):
-	if (body.player == true):
+	if (body.get_name() == "Player"):
 		player_entered = true
