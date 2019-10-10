d088b69cdb5f5071186822e9424b772c4ff19486
valentin-roche <35733063+valentin-roche@users.noreply.github.com>
2019-10-10 08:40:47 +0200

    fix déblocage boss (j'ai 2 IQ)

--- ARPG/ARPG_UI/Planet D.gd
+++ ARPG/ARPG_UI/Planet D.gd
@@ -6,7 +6,7 @@ extends Tabs
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if ButtonState.level7 && ButtonState.level8 && ButtonState.level9 :
+	if ButtonState.level7 || ButtonState.level8 || ButtonState.level9 :
 		$Background3/Boss3.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
